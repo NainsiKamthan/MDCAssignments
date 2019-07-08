2a2c65a9fb8560572b3b00ab5721450ff0fb3cc5
KamthanNainsi <kamthan.nainsi@gmail.com>
2019-07-07 19:12:49 -0500

    Static About Page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is Nainsi Kamthan's Vue Project </h1>
-    <h1>Hello {{ their_name }}, this is an about page</h1>
+    
     <p> This is my first vue project which is used to add and remove skills.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
   </div>
@@ -9,11 +9,6 @@
 
 <script>
 export default {
-  name: 'About',
-  data() {
-    return {
-      their_name: this.$route.params.name
-    }
-  }
+  name: 'About'
 }
 </script>
